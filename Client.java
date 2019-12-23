c9da3a4914ae65f1824d51d1fcc11207d4047d89
tommy21f <57087590+tommy21f@users.noreply.github.com>
2019-12-23 19:38:18 +0200

    Add files via upload

--- Client.java
+++ Client.java
@@ -1,16 +1,63 @@
-	public double bill() {
-		double sum = 0;
-		for(int i = 0; i < insurance.size(); i++) {
-			for(int j = 0; j < Service.services.size(); j++) {
-				if(insurance.get(i)[0][0].equals(Service.services.get(j).getId())) {
-					if (insurance.get(i)[0][1] == 6) {
-						sum = sum + Service.services.get(j).getCost();
-					} else {
-						sum += Service.services.get(j).getCost() * 1.8;
-					}
-					break;
-				}
-			}
-		}
-		return sum;
+import java.util.ArrayList;
+
+public class Client {
+
+	private String name;
+	private String lastName;
+	private int age;
+	private int id;
+	private static int count;
+	private String address;
+	private ArrayList<Integer[][]> insurance = new ArrayList();
+
+	public Client(String name, String lastName, int age, String address, ArrayList<Integer[][]> insurance) {
+		this.name = name;
+		this.lastName = lastName;
+		this.age = age;
+		this.address = address;
+		this.insurance = insurance;
+		count++;
+		id = count;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public ArrayList<Integer[][]> getInsurance() {
+		return insurance;
+	}
+
+	public void setInsurance(ArrayList<Integer[][]> insurance) {
+		this.insurance = insurance;
+	}
+
+}
