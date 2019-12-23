a5a3c43f475f22e34f7cca0710e281412c21fd28
aristotelispapadim <56359822+aristotelispapadim@users.noreply.github.com>
2019-12-23 18:54:36 +0200

    Add files via upload
    Creation of a Client's method
    Method's Name: bill

---
+++ Client.java
@@ -0,0 +1,16 @@
+	public double bill() {
+		double sum = 0;
+		for(int i = 0; i < insurance.size(); i++) {
+			for(int j = 0; j < Service.services.size(); j++) {
+				if(insurance.get(i)[0][0].equals(Service.services.get(j).getId())) {
+					if (insurance.get(i)[0][1] == 6) {
+						sum = sum + Service.services.get(j).getCost();
+					} else {
+						sum += Service.services.get(j).getCost() * 1.8;
+					}
+					break;
+				}
+			}
+		}
+		return sum;
+	}
