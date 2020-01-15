c7057360d8136417149185895e2dc07a644c6d3e
kostasio1313 <46868746+kostasio1313@users.noreply.github.com>
2020-01-15 11:42:55 +0200

    Add files via upload

--- Salesman.java
+++ Salesman.java
@@ -1,165 +1,191 @@
+import java.sql.Statement;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Salesman extends Employee{
+public class Salesman {
 
+	static String text;
+	static String savetext;
 	private int id;
 	private static int count;
 	private double salary;
 	protected static ArrayList<Salesman> salesmen = new ArrayList();
 	protected ArrayList<Client> clients = new ArrayList();
 	Scanner sc = new Scanner(System.in);
-	
-	public Salesman(String name, String lastName, String address, String phoneNumber, String password, double salary, ArrayList<Client> clients) {
-		super(name, lastName, address, phoneNumber, password);
-		this.salary = salary;
-		count++;
-		id = count;
-		this.clients = clients;
-		super.setUsername("s" + id);
-		salesmen.add(this);
-	}
 
-	public Salesman() {
-		super();
-	}
-	
-	public double getSalary() {
-		return salary;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	//log in Ο…Ο€Ξ±Ξ»Ξ»Ξ·Ξ»ΞΏΟ…
-	public static Employee logIn(String username, String password) {
-		for(Employee emp: employee) {
-			if(emp instanceof Salesman) {
-				if(((Salesman) emp).getUsername().equals(username)) 
-					if(((Salesman) emp).getPassword().equals(password)) 
-						
-						return emp;
-			}
+	// Κέρδη ανά πωλητή
+	public static float Salesman_Profit(String username) {
+		String url = "jdbc:sqlserver://195.251.249.161:1433;" + "databaseName=DB35;user=G535;password=48tr93905";
+		float sum = 0;
+		Connection dbcon;
+		Statement stmt1;
+		Statement stmt2;
+		ResultSet rs1;
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		} catch (java.lang.ClassNotFoundException e) {
+			System.out.println("ClassNotFoundException: ");
+			System.out.println(e.getMessage());
 		}
-		return null;
-	}
-	
-	@Override
-	public void menu() {
-		String choice,input;
-		
-		do {
-			System.out.println("1.Προσωπική απόδοση");
-			System.out.println("2.Μισθός και Μπόνους");
-			System.out.println("3.Πελατολόγιο");
-			System.out.println("4.Εγγραφή νέου πελάτη");
-			System.out.print("-->");
-			choice = sc.nextLine();
-			System.out.println();
-		
-			switch(choice) {
-				case "1":
-					System.out.println("H προσωπική σας απόδοση: " + Efficiency());
-				case "2":
-					/*System.out.println("Ο μισθός σας είναι: " + SalesmanProfit() +
-							" και το συνολικό σας μπόνους: " + ExtraSalary());*/
-					System.out.println("Ο μισθός σας είναι: " + getSalary() +
-							"και το συνολικό σας μπόνους: " + ExtraSalary());
-				case "3":
-					System.out.println("ΠΕΛΑΤΟΛΟΓΙΟ");
-					for (Client cli : clients) {
-						System.out.println(cli.getName() + " " + cli.getLastName());
-					}
-				case "4":
-					System.out.print("Εισάγετε το όνομα του πελάτη: ");
-					String name = sc.nextLine();
-					System.out.print("Εισάγετε το επώνυμο του πελάτη: ");
-					String lastName = sc.nextLine();
-					System.out.print("Εισάγετε την ηλικία του πελάτη: ");
-					int age = sc.nextInt();
-					System.out.print("Εισάγετε την διεύθυνση  του πελάτη: ");
-					String address = sc.nextLine();
-					String id;
-					ArrayList<Integer[][]> temp = new ArrayList();
-					do {
-						System.out.println("Εισάγετε το id της ασφάλειας: ");
-						id = sc.nextLine();
-						if (id.equals("")) {		//enter για να σταματήσει να εισάγει ασφάλειες
-							break;
-						} else {
-							String duration;
-							do {
-								System.out.println("Εισάγετε τη διάρκεια της ασφάλειας: ");
-								duration = sc.nextLine();
-								if (id.equals("")) {		//enter για να σταματήσει να εισάγει ασφάλειες
-									break;
-								} else {
-									Integer[][] tempArray = {{Integer.parseInt(id), Integer.parseInt(duration)}};
-									temp.add(tempArray);
-								}
-							} while (true);
-						}
-					} while (true);
-					Client cli = new Client(name, lastName, age, address, temp);
-					clients.add(cli);
-					temp.clear();
-			}
-		}while(true);
-		
-		
-	}
-
-	//Κέρδη ανά πωλητή
-	public double SalesmanProfit() {
-		double sum = 0;
-		for(int i = 0; i < clients.size(); i++) {
-			sum += clients.get(i).bill();
+		try {
+			dbcon = DriverManager.getConnection(url);
+			stmt1 = dbcon.createStatement();
+			stmt2 = dbcon.createStatement();
+			rs1 = stmt1.executeQuery("SELECT sum(bill) as s from Client where salesman='" + username + "'");
+			rs1.next();
+			sum = rs1.getFloat("s");
+			String sql = "UPDATE Salesman SET profit=" + sum + "where Username='" + username + "'";
+			stmt2.executeUpdate(sql);
+			stmt1.close();
+			stmt2.close();
+			rs1.close();
+		} catch (SQLException e) {
+			System.out.print("SQLException: ");
+			System.out.println(e.getMessage());
 		}
 		return sum;
 	}
 
-	//Συνολικά κέρδη επιχείρησης
-	public static double TotalProfit() {
-		double sum = 0;
-		for(int i = 0; i < employee.size(); i++) {
-			if (employee.get(i) instanceof Salesman) {
-				sum += ((Salesman) employee.get(i)).SalesmanProfit();
-			}
+	// Συνολικά κέρδη επιχείρησης
+	public static float Total_Profit() {
+		String url = "jdbc:sqlserver://195.251.249.161:1433;" + "databaseName=DB35;user=G535;password=48tr93905";
+		float sum = 0;
+		Connection dbcon;
+		Statement stmt1;
+		ResultSet rs1;
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		} catch (java.lang.ClassNotFoundException e) {
+			System.out.println("ClassNotFoundException: ");
+			System.out.println(e.getMessage());
+		}
+		try {
+			dbcon = DriverManager.getConnection(url);
+			stmt1 = dbcon.createStatement();
+			rs1 = stmt1.executeQuery("SELECT sum(bill) as s from Client");
+			rs1.next();
+			sum = rs1.getFloat("s");
+			stmt1.close();
+			rs1.close();
+		} catch (SQLException e) {
+			System.out.print("SQLException: ");
+			System.out.println(e.getMessage());
 		}
 		return sum;
 	}
 
-	//Αποδοτικότητα 
-	public double Efficiency() {
-		return this.SalesmanProfit() / TotalProfit();
-		
+	// Αποδοτικότητα
+	public static float Salesman_Efficiency(String username) {
+		float efficiency = (Salesman_Profit(username) / Total_Profit()) * 100;
+		String url = "jdbc:sqlserver://195.251.249.161:1433;" + "databaseName=DB35;user=G535;password=48tr93905";
+		Connection dbcon;
+		Statement stmt1;
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		} catch (java.lang.ClassNotFoundException e) {
+			System.out.println("ClassNotFoundException: ");
+			System.out.println(e.getMessage());
+		}
+		try {
+			dbcon = DriverManager.getConnection(url);
+			stmt1 = dbcon.createStatement();
+			stmt1.executeUpdate("UPDATE Salesman SET efficiency=" + efficiency + "where Username='" + username + "'");
+			stmt1.close();
+		} catch (SQLException e) {
+			System.out.print("SQLException: ");
+			System.out.println(e.getMessage());
+		}
+		return efficiency;
 	}
 
-	//Μπόνους ανα Πωλητή (απο το συνολικο μπατζετ)
-	public double Bonus() {
-		return this.Efficiency() * Manager.getBudget();
-	}
+	public static void Salesman_Clients() {
+		String url = "jdbc:sqlserver://195.251.249.161:1433;" + "databaseName=DB35;user=G535;password=48tr93905;";
+		Connection dbcon;
+		Statement stmt1; /* DataBase returns all Salesmen */
+		ResultSet rs1;
 
-	//Προμηθεια ανα Πωλητή
-	public double Commission() {
-		double sum = 0;
-		for(int i = 0; i < clients.size(); i++) {
-			for(int j = 0; j < clients.get(i).getInsurance().size(); j++) {
-				for(int k = 0; k < Service.services.size(); k++) {
-					if(clients.get(i).getInsurance().get(j)[0][0].equals(Service.services.get(k).getId())) {
-						sum += 0.1 * Service.services.get(j).getCost();
-						break;
-					}
+		try {
+			dbcon = DriverManager.getConnection(url);
+			stmt1 = dbcon.createStatement();
+			rs1 = stmt1.executeQuery("SELECT * FROM Client ");
+			PrintStream output;
+			File file = new File("C:\\Users\\ezisouli\\javaOutputs//clients.txt");
+			output = new PrintStream(file);
+			output.println("Οι πελάτες σας είναι: ");
+			while (rs1.next()) {
+				String salesman = rs1.getString("salesman");
+				if (salesman.equals(LogIn.usernameS)) {
+					String name = rs1.getString("name");
+					String lname = rs1.getString("lastName");
+					int age = rs1.getInt("age");
+					String address = rs1.getString("address");
+					System.setOut(output);
+					output.println("Όνομα Πελάτη: " + name + " Επώνυμο Πελάτη: " + lname + " Ηλικία: " + age
+							+ " Διεύθυνση Κατοικίας: " + address);
 				}
 			}
+			int i = 0;
+			BufferedReader bf = new BufferedReader(new FileReader(file));
+
+			Clients cl = new Clients();
+			while ((text = bf.readLine()) != null) {
+				if (i == 0) {
+					savetext = text + "\n";
+					i++;
+				} else {
+					savetext += text + "\n";
+				}
+			}
+			cl.jTextArea1.setText(savetext);
+			cl.setVisible(true);
+			rs1.close();
+			stmt1.close();
+		} catch (SQLException e) {
+			System.out.print("SQLException: ");
+			System.out.println(e.getMessage());
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		return sum;
 	}
 
-	//Συνολικό Μπόνους ανα πωλητή
-	public double ExtraSalary() {
-		return this.Bonus() + this.Commission();
+	// Συνολικό Μπόνους ανα πωλητή
+	public static float Salesman_ExtraSalary(String username, float bonus_amount) {
+		float commission = (10 / 100 * Salesman_Profit(username));
+		float bonus = bonus_amount * Salesman_Efficiency(username) / 100;
+		float extra_salary = commission + bonus;
+		String url = "jdbc:sqlserver://195.251.249.161:1433;" + "databaseName=DB35;user=G535;password=48tr93905";
+		Connection dbcon;
+		Statement stmt1;
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		} catch (java.lang.ClassNotFoundException e) {
+			System.out.println("ClassNotFoundException: ");
+			System.out.println(e.getMessage());
+		}
+		try {
+			dbcon = DriverManager.getConnection(url);
+			stmt1 = dbcon.createStatement();
+			stmt1.executeUpdate(
+					"UPDATE Salesman SET extra_salary=" + extra_salary + "where Username='" + username + "'");
+			stmt1.close();
+		} catch (SQLException e) {
+			System.out.print("SQLException: ");
+			System.out.println(e.getMessage());
+		}
+		return extra_salary;
 	}
-	
+
 }
